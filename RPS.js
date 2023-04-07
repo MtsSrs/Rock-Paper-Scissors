327666ec724d8cde54f78fac951eaa4b77d292ed
MtsSrs <matheus.1503@hotmail.com>
2023-04-07 18:16:11 -0300

    Feat: Lógica de jogo concluída

--- RPS.js
+++ RPS.js
@@ -1,28 +1,47 @@
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"]
-    const aux = 2
+    const aux = random(0,3)
     return choices[aux];
 }
 
 function playRound(playerSelection, computerSelection) {
-        if(playerSelection.toLowerCase() == computerSelection){
+        if(playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "scissors"){
+            console.warn(`Player choice "${playerSelection}" is not a available choice`);
+        } else if(playerSelection.toLowerCase() == computerSelection){
             console.log("Draw");
+            playerScore++;
+            computerScore++;
         } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock" ) {
             console.log("Player wins!")
+            playerScore++;
         } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper" ){
             console.log("Player wins!")
+            playerScore++;
         } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors"){
             console.log("Player wins!")
+            playerScore++;
         } else {
             console.log("Computer wins!")
+            computerScore++;
         }
 }
-   
+ 
+function game(){
+    let rounds = 0;
 
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
+    while(rounds != 5){
+    let playerSelection = prompt("Rock, Paper or Scissors?")
+    let computerSelection = getComputerChoice();  
+    playRound(playerSelection, computerSelection)
+    rounds++;
+    console.log(`Player Score : ${playerScore} vs ${computerScore} : Computer Score`)
+    }
+        
+}
+
   
-  console.log(playRound(playerSelection, computerSelection));
-
+console.log(game());
